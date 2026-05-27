c03958692f4154d5f8fe6e2c1b415c0506a2011c
James Brook <james.brook4@gmail.com>
2026-05-27 16:58:55 +0200

    detect Cloud Run and Cloud Functions in isCloudEnv() (#11)

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,5 +2,9 @@
 export const isTTY = process.stdout.isTTY || process.env.NODE_ENV === 'test';
 
 export function isCloudEnv() {
-  return !!process.env.KUBERNETES_SERVICE_HOST;
+  return !!(
+    process.env.KUBERNETES_SERVICE_HOST || // GKE
+    process.env.K_SERVICE || // Cloud Run (services and jobs)
+    process.env.FUNCTION_TARGET // Cloud Functions / Cloud Run Functions (gen 2)
+  );
 }
